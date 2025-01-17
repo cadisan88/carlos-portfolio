a00a147e4450959618739b5b4ac09c1079a53897
Carlos Diaz <ca.disan88@gmail.com>
2025-01-17 03:03:11 -0500

    about statistic render list

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
 import Navigation from './components/NavBar.vue'
 import SocialNet from './components/SocialNet.vue'
 import HomeContent from './components/HomeContent.vue'
+import Statistic from './components/AboutStatistic.vue'
+
+const statistics = ref([
+  { id:1, number:'+9', label: 'years of experience as software dev ' },
+  { id:2, number: '2', label: 'fluent languages. English and Spanish' },
+  { id:3, number: '+10', label: 'technologies in my stack' },
+  { id:4, number: '+15', label: 'projects designed, developed and deployed' }
+])
+
 </script>
 
 <template>
@@ -9,6 +18,14 @@ import HomeContent from './components/HomeContent.vue'
     <Navigation/>
     <div class="socialNet"><SocialNet/></div>
     <div class="homeContent"><HomeContent/></div>
+    <div class="statistic">
+      <Statistic
+        v-for="statistic in statistics"
+        :key="statistic.id"
+        :number="statistic.number"
+        :label="statistic.label"
+      ></Statistic>
+    </div>
   </div>
 </template>
 
@@ -31,5 +48,10 @@ import HomeContent from './components/HomeContent.vue'
   display: inline-flex;
   margin-top: 82px;
   margin-bottom: 0px;
+}
+
+.statistic {
+  display: inline-flex;
+  margin-top: 500px;
 }
 </style>
---
+++ src/components/AboutStatistic.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(['number','label'])
+</script>
+
+<template>
+    <div class="statcontainer">
+        <h2 class="datanum">{{ number }}</h2>
+        <h3 class="datatext">{{ label }}</h3>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.statcontainer {
+    background-color: 1E1E1E;
+    box-shadow: 4px 4px #000000 25%;
+}
+</style>
