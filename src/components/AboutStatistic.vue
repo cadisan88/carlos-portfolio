1f7309d9a640ae184ecfff4dd66856786f8c35f2
Carlos Diaz <ca.disan88@gmail.com>
2025-01-17 15:24:41 -0500

    About component done

--- src/components/AboutStatistic.vue
+++ src/components/AboutStatistic.vue
@@ -3,15 +3,45 @@ defineProps(['number','label'])
 </script>
 
 <template>
-    <div class="statcontainer">
-        <h2 class="datanum">{{ number }}</h2>
-        <h3 class="datatext">{{ label }}</h3>
+    <div class="stat-container">
+        <h2>{{ number }}</h2>
+        <h3>{{ label }}</h3>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.statcontainer {
-    background-color: 1E1E1E;
+.stat-container {
+    display: flex;
+    flex-direction: row;
+    background-color: #1B1B1B;
+    border: 1px #1E1E1E solid;
     box-shadow: 4px 4px #000000 25%;
+    height: 289px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    padding: 20px;
+    padding-top: 100px;
+
+    h2 {
+        font-family: 'Roboto';
+        font-weight: 200;
+        font-size: 80px;
+        color: #F3F9F8;
+    }
+
+    h3 {
+        font-family: 'Roboto';
+        font-weight: 100;
+        font-size: 23px;
+        color: #F3F9F8;
+    }
 }
+
+.stat-container:hover {
+    background-color: #ED8404;
+}
+
+
+
 </style>
