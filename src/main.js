16470cb53911d484e83236fed52133907dee2db1
Carlos Diaz <ca.disan88@gmail.com>
2025-01-19 23:18:56 -0500

    Revert "smooth scroll implementation"
    This reverts commit edbbe64264a81a2a8cc31aa3131aa40b7b9d0e82.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
 import './assets/main.css'
-import VueSmoothScroll from 'vue3-smooth-scroll'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const app = createApp(App)
-app.use(VueSmoothScroll, {
-    duration: 1000,
-    updateHistory: false,
-    offset: -82
-})
-app.mount('#app')
+createApp(App).mount('#app')
